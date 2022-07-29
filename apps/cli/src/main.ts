77dbb8d018d3b7b2cd6e176e6c36eeed5c8b866d
Andreas Kollegger <akollegger@gmail.com>
2022-07-29 13:24:54 +0200

    chore(cleanup) minor cleanup of clil commands

--- apps/cli/src/main.ts
+++ apps/cli/src/main.ts
@@ -5,17 +5,19 @@ import { HelloCommand } from './commands/hello/hello.command';
 import { GithubCommand } from './commands/gh/gh.command';
 import { EchoCommand } from './commands/echo/echo.command';
 import { KhorosCommand } from './commands/khoros/khoros.command';
+import { HttpCommand } from './commands/http/http.command';
 
-const movieCLI = build('(c)--(c)')
+const ccCLI = build('neo4j-cc')
   .src(`${__dirname}/`)
-  .plugins('node_modules', { matching: '@c--c/*' })
+  .plugins('node_modules', { matching: '@neo4j-cc/*' })
   .help()
   .version()
   .command(EchoCommand)
   .command(GithubCommand)
   .command(HelloCommand)
   .command(KhorosCommand)
+  .command(HttpCommand)
   .defaultCommand()
   .create()
 
-movieCLI.run()
+ccCLI.run()
--- libs/data-access/github/src/lib/data-access-github.ts
+++ libs/data-access/github/src/lib/data-access-github.ts
@@ -1,7 +1,5 @@
 import { Octokit } from "octokit";
 
-// export const octokit = new Octokit({ auth: `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` });
-
 export interface GithubRequirements {
   auth: string
 }
